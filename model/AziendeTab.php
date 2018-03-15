7cfb55ceb0cca6a9ff01660e547532187b3b375e
Manuel Donati <s_dntmnl99h19g479h@itisurbino.it>
2018-03-15 08:57:40 +0100

    Aggiunto query con JOIN

---
+++ model/AziendeTab.php
@@ -0,0 +1,94 @@
+<?php
+
+class AziendeTab{
+	private $aziende = array();
+
+	//Il costruttore riceve il nome del file sul quale appoggiare i dati
+	function __construct(){
+	}
+
+	public static function getById($id){
+		$query=sprintf("SELECT * FROM Aziende WHERE id = %d", $id);
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$row=$result->fetch_array(MYSQLI_ASSOC);
+			return new Azienda($row['id'],$row['ragioneSociale']);
+		}else{
+			return null;
+		}
+	}
+
+	public static function getAll(){
+		$query=sprintf("SELECT * FROM Aziende");
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$aziende = array();
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$aziende[$row['id']]= new Azienda($row['id'],$row['ragioneSociale']);
+			}
+			return $aziende;
+		}else{
+			return null;
+		}
+	}
+
+
+	public static function remove($azienda){
+		$query=sprintf("DELETE FROM Aziende WHERE id = %d ", $azienda->getId());
+		$result=DBCONNECTION::$con->query($query);
+	}
+
+	public static function insert($azienda){
+		$query=sprintf("INSERT INTO Aziende (ragioneSociale) VALUES('%s')",$azienda->getRagioneSociale());
+		$result=DBCONNECTION::$con->query($query);
+		$n=DBCONNECTION::$con->insert_id;
+		return $n;
+	}
+
+	public static function update($azienda){
+		$query=sprintf("UPDATE Aziende SET ragioneSociale='%s' WHERE id=%d",$azienda->getRagioneSociale(),$azienda->getId());
+		$result=DBCONNECTION::$con->query($query);
+	}
+	
+	public static function getUtenti($azienda){
+		$query=sprintf("SELECT * FROM Utenti WHERE idAzienda=%d",$azienda->getId());
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$utenti = array();
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$dutenti[$row['id']]= new Utente($row['id'],$row['nome'],$row['password'],$row['mail'],$row['idAzienda'],$row['idRuolo']);
+			}
+			return $utenti;
+		}else{
+			return null;
+		}
+	}
+	
+	public static function getRisorse($azienda){
+		$query=sprintf("SELECT * FROM Risorse WHERE idAzienda=%d",$azienda->getId());
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$risorse = array();
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$risorse[$row['id']]= new Risorsa($row['id'],$row['nome'],$row['idAzienda']);
+			}
+			return $risorse;
+		}else{
+			return null;
+		}
+	}
+	
+	public static function getGruppi($azienda){
+		$query=sprintf("SELECT * FROM Gruppi WHERE idAzienda=%d",$azienda->getId());
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$gruppi = array();
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$gruppi[$row['id']]= new Gruppo($row['id'],$row['sigla'],$row['descrizione'],$row['idAzienda']);
+			}
+			return $gruppi;
+		}else{
+			return null;
+		}
+	}
+}
---
+++ model/DispositiviTab.php
@@ -0,0 +1,66 @@
+<?php
+
+class DispositiviTab{
+	private $dispositivi = array();
+
+	//Il costruttore riceve il nome del file sul quale appoggiare i dati
+	function __construct(){
+	}
+
+	public static function getById($id){
+		$query=sprintf("SELECT * FROM Dispositivi WHERE id = %d", $id);
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$row=$result->fetch_array(MYSQLI_ASSOC);
+			return new Dispositivo($row['indirizzoMac'],$row['indirizzoIp'],$row['nome'],$row['orientamento'],$row['idGruppo']);
+		}else{
+			return null;
+		}
+	}
+
+	public static function getAll(){
+		$query=sprintf("SELECT * FROM Dispositivi");
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			$dispositivi = array();
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$dispositivo[$row['id']]= new Dispositivo($row['indirizzoMac'],$row['indirizzoIp'],$row['nome'],$row['orientamento'],$row['idGruppo']);
+			}
+			return $gruppo;
+		}else{
+			return null;
+		}
+	}
+
+
+	public static function remove($dispositivo){
+		$query=sprintf("DELETE FROM Dispositivi WHERE id = %d ", $dispositivo->getId());
+		$result=DBCONNECTION::$con->query($query);
+	}
+
+	public static function insert($dispositivo){
+		$query=sprintf("INSERT INTO Dispositivi (indirizzoMac,indirizzoIp,nome,orientamento,idGruppo) VALUES('%s','%s','%s','%s',%d)",$dispositivo->getIndirizzoMac(),$dispositivo->getIndirizzoIp(),$dispositivo->getNome(),$dispositivo->getOrientamento(),$dispositivo->getIdGruppo());
+		$result=DBCONNECTION::$con->query($query);
+		$n=DBCONNECTION::$con->insert_id;
+		return $n;
+	}
+
+	public static function update($dispositivo){
+		$query=sprintf("UPDATE Dispositivi SET indirizzoMac='%s', indirizzoIp='%s', nome='%s', orientamento='%s', idGruppo=%d WHERE id=%d",$dispositivo->getIndirizzoMac(),$dispositivo->getIndirizzoIp(),$dispositivo->getNome(),$dispositivo->getOrientamento(),$dispositivo->getIdGruppo(),$dispositivo->getId());
+		$result=DBCONNECTION::$con->query($query);
+	}
+
+	public static function getGruppo($dispositivo){
+		$query=sprintf("SELECT * FROM Gruppi WHERE id=%d",$dispositivo->getIdGruppo());
+		$result=DBCONNECTION::$con->query($query);
+		if($result){
+			while($row=$result->fetch_array(MYSQLI_ASSOC)){
+				$gruppo= new Gruppo($row['id'],$row['sigla'],$row['descrizione'],$row['idAzienda']);
+			}
+			return $gruppo;
+		}else{
+			return null;
+		}
+	}
+
+}
