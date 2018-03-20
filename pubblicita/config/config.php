e30ed2c2066662f7d0b86d7d954ffd691151f32e
Lorenzo Maccagli <s_mcclnz99s07z130n@itisurbino.it>
2018-03-20 12:20:42 +0000

    aggiunto path per le immagini

--- pubblicita/config/config.php
+++ pubblicita/config/config.php
@@ -7,6 +7,7 @@
 		public static $controllerPath =  ROOTPATH . "pubblicita/controller/";
 		public static $libPath =  ROOTPATH . "pubblicita/lib/";
 		public static $templatePath =  ROOTPATH . "pubblicita/template/";
+		public static $imgPath =  ROOTPATH . "images/";
 	}
 
 	class DBCONNECTION{
