1dfbf910d9d135e60614fdab282a7273e04362c3
Andrea Matteucci <s_mttndr99d06l500j@itisurbino.it>
2018-03-20 10:43:25 +0100

    creazione template

--- pubblicita/template/main.php
+++ pubblicita/template/main.php
@@ -1,6 +1,74 @@
 <html>
-	<head></head>	
+	<head>
+		 <title>ProjectWork</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    .navbar {
+      margin-bottom: 50px;
+      border-radius: 0;
+    }
+    
+     .jumbotron {
+      margin-bottom: 0;
+    }
+   
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+  </style>
+	</head>	
 	<body>
+	<div class="jumbotron">
+  <div class="container text-center">
+    <h1>ProjectWork</h1>      
+    <p>Digital Signage</p>
+  </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Carica</a></li>
+        <li><a href="#">Ordina</a></li>
+        <li><a href="#">Visualizza</a></li>
+        <li><a href="#">Modifica</a></li>
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Logout</a></li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">    
+  <div class="row">
+    
+  </div>
+</div><br>
+
+</div><br><br>
+
+<footer class="container-fluid text-center">
+	<p>Realizzato dalla classe 5AIN dell' ITIS E.Mattei di Urbino</p>
+</footer>
+	<?php echo $content ?>
 		<?php echo $content ?>
 	</body>
 </html>
