eaf288555ca038ff55e8e89ae713134aea3a19b3
Grassetti Giacomo <s_grsgcm99h28g479w@itisurbino.it>
2018-03-20 09:06:00 +0100

    Sistemazione login

--- pubblicita/model/RuoliTab.php
+++ pubblicita/model/RuoliTab.php
@@ -62,5 +62,5 @@ class RuoliTab{
 		}else{
 			return null;
 		}	
-
+	}
 }
--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,28 @@
 <?php
-	session_start();
+	@session_start();
+
 	include ('../pubblicita/config/config.php');
 	//Recupera il modello sul quale si vuole lavorare
 	$content="";
 	//$model  = (isset($_REQUEST['model']) ? $_REQUEST['model'] : "animali");
-	$model = "";
-
-	if(!isset($_SESSION['id_utente'])){
-		$model="login";
-		$action="login";
+	@$model = $_REQUEST['model'];
+	@$action = $_REQUEST['action'];
+	if(!$_SESSION['id_utente']){
+		if(is_null($_REQUEST['action']) || is_null($_REQUEST['model'])){
+			$model="login";
+			$action="login";
+		}
 	}else{
-		$utente=UtentiTab::getById($_SESSION['id_utente']);
+		if(!@$_REQUEST['action'] || !@$_REQUEST['model']){
+			$model="login";
+			$action="login";
+		}else{
+			$model = $_REQUEST['model'];
+			$action = $_REQUEST['action'];
+			$utente=UtentiTab::getById($_SESSION['id_utente']);
+		}
 	}
+
 	switch ($model) {
 		case 'login':
 			include (CONFIG::$controllerPath.'login/controller.php');
