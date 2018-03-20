5c08bf20be58becc51bd194b6cbce8df0dc12ef3
William Carletti <s_crlwlm99b07l500g@itisurbino.it>
2018-03-20 10:42:45 +0100

    Modifica modelli relativi alle risorse

--- model/Risorsa.php
+++ model/Risorsa.php
@@ -1,5 +1,6 @@
 <?php
 
+include("CreateFiles.php");
 class Risorsa{
 	private $id;
 	private $nome;
@@ -18,9 +19,7 @@ class Risorsa{
 	public function setId($id){
 		$this->id=$id;
 	}
-	public function setId($id){
-		$this->id=$id;
-	}
+
 	public function getId(){
 		return $this->id;
 	}
@@ -66,112 +65,32 @@ class Risorsa{
 		$info = explode(".", $this->nome);
 		switch($info[1]){
 			case 'pdf':
-				echo ($this->convert($this->nome,"./images/",$info[0]));
+				echo (CreateFiles::convert($this->nome,"../images/",$info[0]));
 				break;
 			case 'docx':
 			case 'odt':
-				$this->WordTopPdfConvert($this->nome);
-				echo ($this->convert($info[0].".pdf","./images/",$info[0]));
+				CreateFiles::WordTopPdfConvert($this->nome);
+				echo (CreateFiles::convert($info[0].".pdf","../images/",$info[0]));
 				break;
 		}
 	}
-
-
-		//file existing control
-		public static function controlIfFileExists($fn){return file_exists($fn)?true:false;}
-		
-		//convert from word to pdf
-		public function WordTopPdfConvert($fn){
-			//if (! ( fileTypeConverter::controlIfFileExists($fn))){ return fileTypeConverter::FNE;}
-			try{
-				$result = shell_exec(("export HOME=/tmp && ".$this->CONVERT_COMMAND_WORD_TO_PDF." ".$fn));
-			}catch(Exception $e){
-				$result = $e->getMessage();
-			}
-			return $result;
-		}
-		
-		//convert all pages into .jpg format
-		public function convert($fn, $destination, $name) {
-			//if (! (fileTypeConverter::controlIfFileExists($fn))){ return fileTypeConverter::FNE;}
-			try{
-				$imagick = new Imagick();
-				$imagick->readImage($fn);
-				$imagick->writeImages($destination . $name . ".jpeg", false);
-				$result = true;
-			}catch(Exception $e){
-				$result = $e->getMessage();
-			}
-			return $result;
-		}
 	
-		//count all pages of pdf file
-		public static function countPages($fn){
-			//if (! (fileTypeConverter::controlIfFileExists($fn))){ return fileTypeConverter::FNE;}
-			try{
-				@$pdftext = file_get_contents($fn);
-				$num = preg_match_all("/\/Page\W/", $pdftext, $dummy);
-				$result = $num;
-			}catch(Exception $e){
-				$result = $e->getMessage();
-			}
-			return $result;
-		}
-
-		//convert one single page
-		public function convertPage($page, $fn, $destination, $name){
-			//if (! (fileTypeConverter::controlIfFileExists($fn))){ return fileTypeConverter::FNE;}
-			$page--; //adatto il numero della pagina all'array
-			$command = 'convert ' . $fn . '[' . $page . '] ' . CONFIG::$imagesPath . $name . '.jpeg';
-			try{
-				$result = shell_exec($command);
-			}catch(Exception $e){
-				$result = $e->getMessage();
-			}
-			return $result;
-		}
-
-		//convert just selected pages, thoose written in  array '$pages'
-		public static function convertNPages($pages, $fn, $destination, $name){
-			//if (! (fileTypeConverter::controlIfFileExists($fn))){ return fileTypeConverter::FNE;}
-			$i = 0;
-			$j = 0;
-			$x = 0;
-			$nPages = $this->countPages($fn);
-			//suiting the number of pages into indexes of array
-			for($j=0; $j<count($pages); $j++){
-				$pages[$j]--;
-			}
-			sort($pages);
-			for($i=0; $i<=$nPages; $i++){
-				for($j=0; $j<count($pages); $j++){
-					if($pages[$j]==$i){ 
-						$command = 'convert ' . $fn . '[' . $i . '] ' . CONFIG::$imagesPath . $name . $x . '.jpeg';
-						try{
-							shell_exec($command);
-						}catch(Exception $e){
-							$result = $e->getMessage();
-						}
-						$x++;
-					}
-				}
-			}
-			if($result != null) return $result;
-		}
-		
-		public function saveToDatabase(){
-			$info = explode(".", $this->nome);
-			$n=$this->countPages($info[0].".pdf");
-			if($n==1){
-				$name=$info[0].".jpeg";	
+	public function saveToDatabase(){
+		$info = explode(".", $this->getNome());
+		$n=CreateFiles::countPages("../images/".$this->getNome());
+		if($n==1){
+			$name=$info[0].".jpeg";	
+			$file=new File(null,$name,null, './images/' . $name,$this->id);
+			$file->save();
+		}else{
+			for($i=0;$i<$n;$i++){
+				$name=$info[0]."-".$i.".jpeg";
 				$file=new File(null,$name,null, './images/' . $name,$this->id);
 				$file->save();
-			}else{
-				for($i=0;$i<$n;$i++){
-					$name=$info[0]."-".$i.".jpeg";
-					$file=new File(null,$name,null, './images/' . $name,$this->id);
-					$file->save();
-				}
 			}
 		}
+	}
+	
+	
+
 }
