043c131f0d78c0005f74acb6757339b441cb14d4
Giacomo Grassi <s_grsgcm98p22l500b@itisurbino.it>
2018-03-21 09:27:09 +0100

    sistemazione

--- pubblicita/config/config.php
+++ pubblicita/config/config.php
@@ -1,17 +1,14 @@
 <?php
 	define("ROOTPATH", __DIR__."/../../");
-	
-	
+
+
 	class CONFIG{
 		public static $modelPath =  ROOTPATH . "pubblicita/model/";
 		public static $controllerPath =  ROOTPATH . "pubblicita/controller/";
 		public static $libPath =  ROOTPATH . "pubblicita/lib/";
 		public static $templatePath =  ROOTPATH . "pubblicita/template/";
-<<<<<<< HEAD
-		public static $imgPath =  ROOTPATH . "images/";
-=======
 		public static $imagesPath =  ROOTPATH . "public_html/images/";
->>>>>>> ff2a7e1b0083e95fdd709ff946ab610f94f302f2
+
 	}
 
 	class DBCONNECTION{
@@ -26,8 +23,8 @@
 	/*
 	 * Creazione tabella di TEST
 	 */
-	
-	
+
+
 	/*
 	 * Include delle librerie e dei modelli
 	 */
