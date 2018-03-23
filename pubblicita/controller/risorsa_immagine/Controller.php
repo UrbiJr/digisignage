a69653e1b6c2fe2967ae2e0e201d424b18f9b68d
William Carletti <s_crlwlm99b07l500g@itisurbino.it>
2018-03-23 10:46:15 +0100

    Modifica controller

--- pubblicita/controller/risorsa_immagine/Controller.php
+++ pubblicita/controller/risorsa_immagine/Controller.php
@@ -1,39 +1,33 @@
 <?php
-
+	
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
-
-
 	$action=$_REQUEST['action'];
 	switch ($action){
 		case 'new':
-			$content=get_include_contents(CONFIG::$controllerPath."/risorsa_immagine/FormAggiungi.php");
-			break;
+			$content=get_include_contents("./FormAggiungi.php");
 		case 'add':
 			$file=basename($_FILES['file']['name']);
 			$risorsa=new Risorsa(null,$file,1);
 			$risorsa->save();
-			$content = get_include_contents(CONFIG::$controllerPath."risorsa_documento/ok.php");
 			break;
-
-		case 'delete':
-
+		
+		case 'delete':			
 			break;
-
+		
 		case 'edit':
 			break;
 
 		case 'update':
-			break;
-
+			break;	
+			
 		case 'list':
 			$risorse = RisorseTab::getAll();
-			//$content=get_include_contents(/*Path::$controllerPath.*/"ViewRisorse.php");
-			include("ViewRisorse.php");
+			$content=get_include_contents("./ViewRisorse.php");
 			break;
-
+			
 		default:
 			echo 'oh no';
 			break;
