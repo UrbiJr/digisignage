0b783ceec114ade2fe9af19639a2b249ca048f3a
Giacomo Grassi <s_grsgcm98p22l500b@itisurbino.it>
2018-04-05 13:16:29 +0200

    ciao

--- pubblicita/controller/risorsa_documento/ViewAggiungi.php
+++ pubblicita/controller/risorsa_documento/ViewAggiungi.php
@@ -1,24 +1,32 @@
 <html>
 	<head>
 		<title>Carica Documento</title>
-	
+		<style>
+	     input[type='file'] {
+  				color: transparent;
+				}
+
+
+
+	    </style>
 
 	</head>
 	<body>
 
+	<form>
 		<div class="row">
  			<div class="col-sm-4"></div>
  			<div class="col-sm-4">
-				<center>
-					<form action="index.php?model=risorsa_documento&action=add" method="post" enctype="multipart/form-data">
-						Seleziona immagine:
-						<input type="file" name="file"/>
-						<input type="hidden" name="action" value="add"/>
-						<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+
+						<div class="form-group">
+		 					<label for="email">Seleziona immagine:</label>
+		 					<input type="file" name="file"/>
+ 							<input type="hidden" name="action" value="add"/>
+ 							<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+	 					</div>
 						<input type="submit" class="btn btn-dark" value="Aggiungi" name="submit"/>
-						
 					</form>
-				</center>
+				
 			</div>
  			<div class="col-sm-4"></div>
 		</div>
