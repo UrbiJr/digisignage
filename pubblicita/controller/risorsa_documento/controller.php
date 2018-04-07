a008d6de194ebe7814bc1a7b7096aa043c50f1f0
Federico Urbinelli <s_rbnfrc97p24l500h@itisurbino.it>
2018-04-07 20:47:50 +0200

    RIPRISTINO VECCHIA VERSIONE FUNZIONANTE CON INTEGRAZIONE DEI GIUSTI FILE AGGIORNAT

--- pubblicita/controller/risorsa_documento/controller.php
+++ pubblicita/controller/risorsa_documento/controller.php
@@ -1,56 +1,45 @@
 <?php
-
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-
 	//include("../../config/config.php");
 	$action=$_REQUEST['action'];
-
 	$action=$_REQUEST['action'];
 	switch ($action){
 		case 'new':
 			$content=get_include_contents(CONFIG::$controllerPath."risorsa_documento/ViewAggiungi.php");
 			break;
-
 		case 'add':
-			$target_dir = CONFIG::$controllerPath."risorsa_documento/temp/";
-			$target_file = $target_dir . basename($_FILES['file']['name']);
+			$target_dir = "/tmp/";
+			$target_file = $target_dir . basename($_FILES['file']['tmp_name']);
 			//$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-			
-			echo($_FILES["file"]["tmp_name"]);
-			if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-				echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-			}else{
+
+			//echo($_FILES["file"]["tmp_name"]);
+			define("TEMP_NAME",$_FILES["file"]["tmp_name"]);
+			if (!(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file))) {
 				echo("Failed to upload.");
 			}
-			
-			$risorsa=new Risorsa(null,$target_file,1);
+
+			$risorsa=new Risorsa(null,basename($_FILES['file']['name']),$utente->getIdAzienda());
 			$risorsa->save();
 			$content = get_include_contents(CONFIG::$controllerPath."risorsa_documento/ok.php");
 			break;
-
 		case 'delete':
-			$risorsa = RisorseTab::getById($_GET['id']);
+			$risorsa=RisorseTab::getById($_GET['id']);
 			RisorseTab::remove($risorsa);
-			$risorse = RisorseTab::getRisorseByUtente($utente);
+			$risorse=RisorseTab::getRisorseByUtente($utente);
 			$content=get_include_contents(CONFIG::$controllerPath."risorsa_documento/ViewRisorse.php");
 			break;
-
 		case 'edit':
 			break;
-
 		case 'update':
 			break;
-
 		case 'list':
 			$risorse = RisorseTab::getRisorseByUtente($utente);
 			$content=get_include_contents(CONFIG::$controllerPath."risorsa_documento/ViewRisorse.php");
 			break;
-
 		default:
 			echo 'oh no';
 			break;
 	}
-
 ?>
--- pubblicita/model/Risorsa.php
+++ pubblicita/model/Risorsa.php
@@ -1,12 +1,9 @@
 <?php
-
 include("CreateFiles.php");
-
 class Risorsa{
 	private $id;
 	private $nome;
 	private $idAzienda;
-
 	/* 	costruttore per Risorse create per la prima volta
 		OPPURE (ri)create dalle query (es. GruppiTab::getRisorse()).
 		Per risorse create per la prima volta, passare null come
@@ -21,85 +18,73 @@ class Risorsa{
 			$this->controllaTipoRisorsa();
 		}
 	}
-
 	public function setId($id){
 		$this->id=$id;
 	}
-
 	public function getId(){
 		return $this->id;
 	}
-
 	public function getNome(){
 		return $this->nome;
 	}
-
 	public function setNome($nome){
 		$this->nome = $nome;
 	}
-
 	public function getIdAzienda(){
 		return $this->idAzienda;
 	}
-
 	public function setIdAzienda($idAzienda){
 		$this->nome = $idAzienda;
 	}
-
-
 	public function save(){
 		if(!$this->id){
-			echo getcwd();
 			$n=RisorseTab::insert($this);
 			$this->setId($n);
-			return true;
 		}else{
 			RisorseTab::update($this);
-			return true;
 		}
-		return false;
+		$this->saveToDatabase();
 	}
-
 	public function delete(){
 		RisorseTab::remove($this);
 	}
-
 	public function getFile(){
 		return RisorseTab::getFile($this);
 	}
-
 	function controllaTipoRisorsa(){
+		$tmp_name=explode('/',TEMP_NAME);
 		$info = explode(".", $this->nome);
 		switch($info[1]){
 			case 'pdf':
-				echo (CreateFiles::convert($this->nome,"/images/",$info[0]));
-				$this->saveToDatabase($info[1]);
+				echo (CreateFiles::convert(TEMP_NAME,"./images/",$info[0]));
 				break;
-			case 'docx':
-			case 'odt':
-				CreateFiles::WordToPdfConvert($this->nome);
-				echo (CreateFiles::convert($info[0].".pdf","/images/",$info[0]));
-				break;
+			default:
+				rename((TEMP_NAME), ("./images/".$this->nome));
 		}
 	}
-
-	private function saveToDatabase($fileExt){
-		if($fileExt==='pdf'){
-			$n=CreateFiles::countPages($info[0].".pdf");
+	private function saveToDatabase(){
+		$info = explode(".", $this->nome);
+		if($info[1]==='pdf'){
+			$n=CreateFiles::countPages(TEMP_NAME);
 			if($n==1){
-				$name=$info[0].".jpeg";
+				$name="1.jpeg";
 				$file=new File(null,$name,null, './images/' . $name,$this->id);
 				$file->save();
 			}else{
 				for($i=0;$i<$n;$i++){
-					$name=$info[0]."-".$i.".jpeg";
+					$name=$i.".jpeg";
 					$file=new File(null,$name,null, './images/' . $name,$this->id);
 					$file->save();
 				}
 			}
 		}else{
-			$file=new File(null,$this->getNome(),null, './images/' . $this->getNome(),$this->id);
+			$name="1.jpeg";
+			$file=new File(null,$this->name,null, './images/' . $this->getNome(),$this->id);
 			$file->save();
 		}
+	}
+
+	private function MakeDir($name){
+
 	}
 }
