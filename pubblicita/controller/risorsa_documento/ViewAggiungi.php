e97c60b970c8701ad70087e8f46f367acc2974e8
Giacomo Grassi <s_grsgcm98p22l500b@itisurbino.it>
2018-04-11 11:37:52 +0200

    ciao

--- pubblicita/controller/risorsa_documento/ViewAggiungi.php
+++ pubblicita/controller/risorsa_documento/ViewAggiungi.php
@@ -24,19 +24,17 @@
  			<div class="col-sm-4">
 				<center>
 				<br>
-					<h1><i>Carica risorsa</i></h1> <br>
 					<h3>Seleziona la risorsa da caricare </h3> <br>
 					<form action="index.php?model=risorsa_documento&action=add" method="post" enctype="multipart/form-data">
 						<p>
 							<input type="file" class="btn btnUpload" name="file" onchange="javascript: document.getElementById ('fileName') . value = this.value"/>
-							<input type="hidden" name="action" value="add"/> 
+							<input type="hidden" name="action" value="add"/>
 							<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
 						</p>
 						<p>
 							<input type="submit" class="btn btn-dark" value="Aggiungi" name="submit" />
 						</p>
 					</form>
-<<<<<<< HEAD
 					<p> <?php echo $error ?> </p>
 
 			</div>
@@ -44,9 +42,8 @@
 		</div>
 
 	</div>
-=======
 					<br>
 					<h2><?php echo $error ?> </h2>
 				</center>
->>>>>>> bb1a90256fb2a89e4eee1d4a1f27f58468318b2f
+
 </html>
