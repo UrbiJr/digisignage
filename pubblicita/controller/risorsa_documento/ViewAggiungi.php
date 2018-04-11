703c8f3105ebeed920a7f96ecb1c68cd9b959680
Tommaso Labieni <s_lbntms99e04g479p@itisurbino.it>
2018-04-11 12:29:35 +0200

    Risoluzione problema visualizzazione pagina aggiunta risorsa

--- pubblicita/controller/risorsa_documento/ViewAggiungi.php
+++ pubblicita/controller/risorsa_documento/ViewAggiungi.php
@@ -1,20 +1,8 @@
-
-<div class="container-fluid">
-<div class="row">
-	<div class="col-sm-4"></div
-		<div class="col-sm-4">
-			<center>
-				<h1 class="display-4">Carica Risorsa</h1>
-			</center>
-		</div>
-	<div class="col-sm-4"></div>
-</div>
-
 <html>
 	<head>
 		<title>Carica Documento</title>
-		<script type="text/javascript" src="js/bootstrap-filestyle.min.js"> </script>
 		<link rel="stylesheet" type="text/css" href="css/ViewAggiungi.css">
+		<script type="text/javascript" src="js/bootstrap-filestyle.min.js"> </script>
 	</head>
 	<body>
 
@@ -27,8 +15,8 @@
 					<h1><i>Carica risorsa</i></h1> <br>
 					<h3>Seleziona la risorsa da caricare </h3> <br>
 					<form action="index.php?model=risorsa_documento&action=add" method="post" enctype="multipart/form-data">
+<div><input type='file' class="btn btnUpload"title="Choose a video please" id="aa" onchange="pressed()"><label id="fileLabel"></label></div>
 						<p>
-							<input type="file" class="btn btnUpload" name="file" onchange="javascript: document.getElementById ('fileName') . value = this.value"/>
 							<input type="hidden" name="action" value="add"/> 
 							<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
 						</p>
@@ -37,6 +25,7 @@
 						</p>
 					</form>
 					<br>
-					<h2><?php echo $error ?> </h2>
+					<h2><?php echo $error; ?> </h2>
 				</center>
+
 </html>
